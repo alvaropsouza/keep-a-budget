d485649bd98db6b128f3a4f306c0405e30ecd3c2
Álvaro Souza <alvaropatricio61@gmail.com>
2026-06-09 17:11:36 -0300

    feat: remover módulo e funcionalidades relacionadas a metas financeiras

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { InvoicesModule } from "./modules/invoices.module";
 import { ExpensesModule } from "./modules/expenses.module";
 import { FixedExpensesModule } from "./modules/fixed-expenses.module";
 import { FreelanceInvoiceModule } from "./modules/freelance-invoice.module";
-import { FinancialGoalModule } from "./modules/financial-goal.module";
 import { CacheModule } from "./modules/cache.module";
 import { AiModule } from "./modules/ai.module";
 import { IrDocumentModule } from "./modules/ir-document.module";
@@ -21,7 +20,6 @@ import { BudgetModule } from "./modules/budget.module";
     ExpensesModule,
     FixedExpensesModule,
     FreelanceInvoiceModule,
-    FinancialGoalModule,
     AiModule,
     IrDocumentModule,
     BudgetModule,
